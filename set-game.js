b2393a2d848f755bdd77708786b31e8e219dedca
Erick Felipa <erickfelipa@gmail.com>
2024-05-27 11:29:06 -0500

    Turning the game in console into a UI-game

--- set-game.js
+++ set-game.js
@@ -1,67 +1,31 @@
+const root = document.querySelector("#root");
+const rockButton = document.createElement('button');
+const paperButton = document.createElement('button');
+const scissorsButton = document.createElement('button');
+let humanSelection = '';
+
+
+rockButton.textContent = 'Rock';
+paperButton.textContent = 'Paper';
+scissorsButton.textContent = 'Scissors';
+rockButton.setAttribute('class', 'game-button');
+paperButton.setAttribute('class', 'game-button');
+scissorsButton.setAttribute('class', 'game-button');
+
+root.appendChild(rockButton);
+root.appendChild(paperButton);
+root.appendChild(scissorsButton);
+
+rockButton.addEventListener('click', () => {
+    humanSelection = rockButton.textContent;
+})
+
 const startGame = () => {
-    console.log('Hello world!');
-    const OPTIONS = ['rock', 'paper', 'scissors'];
-    let computerScore = 0;
-    let humanScore = 0;
-
-    for(let i = 0; i < 6; i++) {
-        const humanSelection = getHumanChoice(OPTIONS);
-        const computerSelection = getComputerChoice(OPTIONS);
-        const didThePlayerWin = playRound(humanSelection, computerSelection);
-        (didThePlayerWin) ? humanScore++ : computerScore++;
-        if (computerScore + humanScore === 5) {
-            alert('Game over');
-            if(computerScore > humanScore) alert('Computer wins.');
-            if(humanScore > computerScore) alert('You win the game. Congratulations.');
-        }
-    }
-}
-
-const playRound = (humanChoice, computerChoice) => {
-    if(!humanChoice) {
-        startGame();
+    if(humanSelection !== '') {
+        alert(`Game starting`)
     } else {
-        if(humanChoice === computerChoice) {
-            alert(`Draw!!! Both player selected ${humanChoice}`);
-            alert(`Round over`)
-        } else if((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'paper' && computerChoice === 'rock') || (humanChoice === 'scissors' && computerChoice === 'paper')) {
-            alert(`You won! ${humanChoice} beats ${computerChoice}`);
-            alert(`Round over`)
-            return true;
-        } else {
-            alert(`You lost! ${computerChoice} beats ${humanChoice}`);
-            alert(`Round over`);
-            return false;
-        }
+        throw new Error('Game is not starting')
     }
 }
 
-const getComputerChoice = (OPTIONS) => {
-    let randomizer = Math.floor(Math.random() * OPTIONS.length);
-
-    if(randomizer > OPTIONS.length || randomizer < 0) {
-        getComputerChoice(OPTIONS);
-    } else {
-        console.log(OPTIONS[randomizer]);
-        return OPTIONS[randomizer];
-    }
-}
-
-const getHumanChoice = (OPTIONS) => {
-    let humanChoice = prompt('Please choice between rock, paper and scissors', '').toLowerCase();
-    if(humanChoice === '') {
-        alert('Please choose an option before start the game.');
-        getHumanChoice(OPTIONS);
-    }
-    if (typeof humanChoice !== 'string') {
-        alert('Please enter a valid choice.');
-        getHumanChoice();
-    } 
-    if (!OPTIONS.includes(humanChoice)) {
-        alert(`${humanChoice} is not a valid choice. Please choose between rock, paper and scissors.`)
-    } else {
-        return humanChoice;
-    }
-}
-
-addEventListener('DOMContentLoaded', startGame)
+startGame();
