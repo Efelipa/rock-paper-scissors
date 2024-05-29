61e1f71b7c665b1b78e8161943746eb916bd1dc5
Erick Felipa <erickfelipa@gmail.com>
2024-05-29 12:39:08 -0500

    Result div on progres

--- set-game.js
+++ set-game.js
@@ -2,8 +2,10 @@ const root = document.querySelector("#root");
 const rockButton = document.createElement('button');
 const paperButton = document.createElement('button');
 const scissorsButton = document.createElement('button');
-let humanSelection = '';
-
+const buttonsDiv = document.querySelector('.buttons');
+const resultsDiv = document.querySelector('.results');
+const OPTIONS = ['rock', 'paper', 'scissors'];
+let selection;
 
 rockButton.textContent = 'Rock';
 paperButton.textContent = 'Paper';
@@ -12,20 +14,69 @@ rockButton.setAttribute('class', 'game-button');
 paperButton.setAttribute('class', 'game-button');
 scissorsButton.setAttribute('class', 'game-button');
 
-root.appendChild(rockButton);
-root.appendChild(paperButton);
-root.appendChild(scissorsButton);
+// Adding the buttons to the HTML page
+buttonsDiv.appendChild(rockButton);
+buttonsDiv.appendChild(paperButton);
+buttonsDiv.appendChild(scissorsButton);
 
-rockButton.addEventListener('click', () => {
-    humanSelection = rockButton.textContent;
-})
+
+const getComputerChoice = options => {
+    //Choose a random number between the length of the options that we have.
+    // return the string with a specific value with randomizer.
+    const randomizer = Math.ceil(Math.random() * options.length) - 1;
+    return options[randomizer];
+}
+
+
+const getHumanChoice = () => {
+    // I select all the button options. 
+    const buttons = document.querySelectorAll('button');
+    //For each one, when clicking on them will assign their textContent to the selection variable and start the game. 
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            selection = button.textContent.toLowerCase();
+            startGame();
+        });
+    });
+}
 
 const startGame = () => {
-    if(humanSelection !== '') {
-        alert(`Game starting`)
+    if (humanSelection) {
+        const didPlayerWin = playRound(humanSelection, computerSelection);
+        for(let i = 0; i < 5; i++) {
+            const humanSelection = selection;
+            const computerSelection = getComputerChoice(OPTIONS);
+            
+        }
     } else {
-        throw new Error('Game is not starting')
+        console.log('Start the game');
     }
 }
 
-startGame();
+const playRound = (humanChoice, computerChoice) => {
+    if(!humanChoice) {
+        getHumanChoice();
+    } else {
+        if(humanChoice === computerChoice) {
+            resultsDiv.textContent = 'DRAW';
+            resultsDiv.classList.add('draw');
+            resultsDiv.classList.remove('victory');
+            resultsDiv.classList.remove('defeat');
+        } else if((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'paper' && computerChoice === 'rock') || (humanChoice === 'scissors' && computerChoice === 'paper')) {
+            resultsDiv.textContent = `YOU WON! ${humanChoice} beats ${computerChoice}`;
+            resultsDiv.classList.remove('draw');
+            resultsDiv.classList.add('victory');
+            resultsDiv.classList.remove('defeat');
+            return true;
+        } else {
+            resultsDiv.textContent = `YOU LOSE! ${computerChoice} beats ${humanChoice}`;
+            resultsDiv.classList.remove('draw');
+            resultsDiv.classList.remove('victory');
+            resultsDiv.classList.add('defeat');
+            return false;
+        }
+    }
+
+}
+
+getHumanChoice();
