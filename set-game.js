c8d705045d25ea7653e6c7de068fc6bd835a0658
Erick Felipa <erickfelipa@gmail.com>
2024-05-30 09:20:18 -0500

    Se creo una interfaz visual basica

--- set-game.js
+++ set-game.js
@@ -1,67 +1,19 @@
-const startGame = () => {
-    console.log('Hello world!');
-    const OPTIONS = ['rock', 'paper', 'scissors'];
-    let computerScore = 0;
-    let humanScore = 0;
+let buttonText = '';
+const root = document.querySelector('#root');
+const buttons = document.querySelectorAll('#buttons');
 
-    for(let i = 0; i < 6; i++) {
-        const humanSelection = getHumanChoice(OPTIONS);
-        const computerSelection = getComputerChoice(OPTIONS);
-        const didThePlayerWin = playRound(humanSelection, computerSelection);
-        (didThePlayerWin) ? humanScore++ : computerScore++;
-        if (computerScore + humanScore === 5) {
-            alert('Game over');
-            if(computerScore > humanScore) alert('Computer wins.');
-            if(humanScore > computerScore) alert('You win the game. Congratulations.');
-        }
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        buttonText = e.target.textContent;
+    });
+});
+
+const getHumanChoice = () => {
+    if(buttonText === 'Rock') {
+        console.log('Work')
     }
-}
+};
 
-const playRound = (humanChoice, computerChoice) => {
-    if(!humanChoice) {
-        startGame();
-    } else {
-        if(humanChoice === computerChoice) {
-            alert(`Draw!!! Both player selected ${humanChoice}`);
-            alert(`Round over`)
-        } else if((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'paper' && computerChoice === 'rock') || (humanChoice === 'scissors' && computerChoice === 'paper')) {
-            alert(`You won! ${humanChoice} beats ${computerChoice}`);
-            alert(`Round over`)
-            return true;
-        } else {
-            alert(`You lost! ${computerChoice} beats ${humanChoice}`);
-            alert(`Round over`);
-            return false;
-        }
-    }
-}
+const humanSelection = getHumanChoice();
 
-const getComputerChoice = (OPTIONS) => {
-    let randomizer = Math.floor(Math.random() * OPTIONS.length);
 
-    if(randomizer > OPTIONS.length || randomizer < 0) {
-        getComputerChoice(OPTIONS);
-    } else {
-        console.log(OPTIONS[randomizer]);
-        return OPTIONS[randomizer];
-    }
-}
-
-const getHumanChoice = (OPTIONS) => {
-    let humanChoice = prompt('Please choice between rock, paper and scissors', '').toLowerCase();
-    if(humanChoice === '') {
-        alert('Please choose an option before start the game.');
-        getHumanChoice(OPTIONS);
-    }
-    if (typeof humanChoice !== 'string') {
-        alert('Please enter a valid choice.');
-        getHumanChoice();
-    } 
-    if (!OPTIONS.includes(humanChoice)) {
-        alert(`${humanChoice} is not a valid choice. Please choose between rock, paper and scissors.`)
-    } else {
-        return humanChoice;
-    }
-}
-
-addEventListener('DOMContentLoaded', startGame)
